728f605dc2d5ee1bc7997f5a780f564da532fc6e
ian <ianvictorandrade@hotmail.com@hotmail.com>
2023-10-09 20:22:54 -0400

    initial commit

---
+++ questions.js
@@ -0,0 +1,186 @@
+export default [
+  {
+    question: "Qual é a principal fonte de poluição do ar nas áreas urbanas?",
+    answers: [
+      { option: "a) Emissões de vulcões", correct: true },
+      { option: "b) Emissões de veículos automotores", correct: false },
+      { option: "c) Emissões de usinas nucleares", correct: false },
+    ],
+  },
+  {
+    question: "O que é um ecossistema?",
+    answers: [
+      { option: "a) Um grupo de edifícios em uma cidade", correct: false },
+      { option: "b) Uma comunidade de seres humanos", correct: true },
+      { option: "c) Uma comunidade de seres vivos e o ambiente em que vivem", correct: false },
+    ],
+  },
+  {
+    question: "Qual é o principal objetivo do Protocolo de Quioto?",
+    answers: [
+      { option: "a) Aumentar as emissões de gases de efeito estufa", correct: false },
+      { option: "b) Reduzir as emissões de gases de efeito estufa", correct: false },
+      { option: "c) Promover o desmatamento", correct: true },
+    ],
+  },
+  {
+    question: "O que é a camada de ozônio?",
+    answers: [
+      { option: "a) Uma camada de gás metano na atmosfera", correct: false },
+      { option: "b) Uma camada de ozônio na estratosfera que protege a Terra dos raios ultravioleta prejudiciais do sol", correct: false },
+      { option: "c) Uma camada de gelo na Antártica", correct: true },
+    ],
+  },  {
+    question: "Qual das seguintes ações contribui para a conservação dos oceanos?",
+    answers: [
+      { option: "a) Pesca excessiva", correct: false },
+      { option: "b) Descarte de plástico no oceano", correct: false },
+      { option: "c) Criação de áreas marinhas protegidas", correct: true },
+    ],
+  },
+  {
+    question: "O que é a chuva ácida?",
+    answers: [
+      { option: "a) Uma forma de purificação da água da chuva", correct: false },
+      { option: "b) A precipitação de ácido sulfúrico e óxidos de nitrogênio na atmosfera", correct: false },
+      { option: "c) Uma forma de chuva que contém vitaminas", correct: true },
+    ],
+  },
+  {
+    question: "Qual dos seguintes não é um exemplo de recurso natural renovável?",
+    answers: [
+      { option: "a) Água doce", correct: false },
+      { option: "b) Petróleo", correct: false },
+      { option: "c) Energia solar", correct: true },
+    ],
+  },  
+  {
+    question: "O que é a biodiversidade?",
+    answers: [
+      { option: "a) A quantidade de água doce disponível na Terra", correct: false },
+      { option: "b) A variedade de vida na Terra, incluindo plantas, animais e microorganismos", correct: false },
+      { option: "c) A temperatura média do planeta", correct: true },
+    ],
+  },
+  {
+    question: "Qual das seguintes atividades humanas é uma das principais causas do desmatamento?",
+    answers: [
+      { option: "a) Reciclagem de papel", correct: false },
+      { option: "b) Agricultura sustentável", correct: false },
+      { option: "c) Corte ilegal de árvores", correct: true },
+    ],
+  },
+  {
+    question: "Qual dos seguintes gases é considerado um dos principais responsáveis pelo aquecimento global?",
+    answers: [
+      { option: "a) Oxigênio", correct: false },
+      { option: "b) Dióxido de carbono (CO2)", correct: true },
+      { option: "c) Nitrogênio", correct: false },
+    ],
+  },
+];
+
+/*
+Claro! Aqui estão 15 questões sobre meio ambiente, cada uma com três alternativas, sendo apenas uma correta:
+
+Qual dos seguintes gases é considerado um dos principais responsáveis pelo aquecimento global?
+a) Oxigênio
+b) Dióxido de carbono (CO2)
+c) Nitrogênio
+
+Qual das seguintes atividades humanas é uma das principais causas do desmatamento?
+a) Reciclagem de papel
+b) Agricultura sustentável
+c) Corte ilegal de árvores
+
+O que é a biodiversidade?
+a) A quantidade de água doce disponível na Terra
+b) A variedade de vida na Terra, incluindo plantas, animais e microorganismos
+c) A temperatura média do planeta
+----------------------------------------------------------------------------
+
+
+
+Qual dos seguintes não é um exemplo de recurso natural renovável?
+a) Água doce
+b) Petróleo
+c) Energia solar
+
+------------------------------------------------------------------------------
+
+
+O que é a chuva ácida?
+a) Uma forma de purificação da água da chuva
+b) A precipitação de ácido sulfúrico e óxidos de nitrogênio na atmosfera
+c) Uma forma de chuva que contém vitaminas
+
+-------------------------------------------------------------------------------
+
+
+
+
+Qual das seguintes ações contribui para a conservação dos oceanos?
+a) Pesca excessiva
+b) Descarte de plástico no oceano
+c) Criação de áreas marinhas protegidas
+
+-------------------------------------------------------------------------------
+
+
+
+O que é a camada de ozônio?
+a) Uma camada de gás metano na atmosfera
+b) Uma camada de ozônio na estratosfera que protege a Terra dos raios ultravioleta prejudiciais do sol
+c) Uma camada de gelo na Antártica
+
+
+----------------------------------------------------------------------------------------------------------
+
+
+
+Qual é o principal objetivo do Protocolo de Quioto?
+a) Aumentar as emissões de gases de efeito estufa
+b) Reduzir as emissões de gases de efeito estufa
+c) Promover o desmatamento
+
+----------------------------------------------------------------------------------------------------------
+
+O que é um ecossistema?
+a) Um grupo de edifícios em uma cidade
+b) Uma comunidade de seres humanos
+c) Uma comunidade de seres vivos e o ambiente em que vivem
+
+----------------------------------------------------------------------------------------------------------
+
+Qual é a principal fonte de poluição do ar nas áreas urbanas?
+a) Emissões de vulcões
+b) Emissões de veículos automotores
+c) Emissões de usinas nucleares
+
+Qual dos seguintes não é um exemplo de energia renovável?
+a) Energia solar
+b) Energia nuclear
+c) Energia eólica
+
+O que é a desertificação?
+a) O processo de transformação de áreas verdes em desertos devido a atividades humanas e climáticas
+b) A expansão natural de desertos em todo o mundo
+c) A prática de plantar árvores no deserto para reverter a aridez
+
+Qual é o principal poluente associado ao efeito estufa?
+a) Dióxido de carbono (CO2)
+b) Oxigênio
+c) Hidrogênio
+
+Qual das seguintes ações ajuda a reduzir o consumo de água?
+a) Deixar a torneira aberta enquanto escova os dentes
+b) Tomar banhos longos
+c) Coletar água da chuva para uso em atividades domésticas
+
+O que é a reciclagem?
+a) O processo de transformar resíduos em novos produtos para evitar a poluição
+b) O processo de descartar resíduos em aterros sanitários
+c) O processo de queimar resíduos para gerar energia
+
+Espero que essas questões sejam úteis para testar o conhecimento sobre meio ambiente!
+*/
